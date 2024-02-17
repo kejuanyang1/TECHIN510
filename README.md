afe5421f059dab4a9a4c90099462d5a7e5406797
kejuanyang1 <1298720064@qq.com>
2024-02-16 20:00:01 -0800

    llm

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# TECHIN 510 LAB 1
-Kejuan Yang's Personal Website
+# TECHIN 510 LAB 6
+AI Resume Bot
 
 ## Getting Started
 Open the terminal and run the following commands:
@@ -18,19 +18,13 @@ streamlit run app.py
 ```
 
 ## What's Included
-- ```app.py```: The main Flask application
-- `imgs/`: A directory containing images used by the application.
+- ```app.py```: The main streamlit application
 - `README.md`: The markdown file providing a descriptive overview of the project, setup instructions, and additional information.
 - `requirements.txt`: A text file listing the Python dependencies for the project, which can be installed using `pip`.
 
 ## Lessons Learned
-- I learned the logic flow of how to build and host a website using Azure app service, sync with a Github repo. When deploying the website, the envrionment ```.venv``` cannot be included in the repo, otherwise the deployment would fail. It takes around 5 minutes to build and deploy everytime I update the website.
-- I practiced how to use Streamlit to build the frontend page. It shares the similar logic with markdown.
+- I learned and practiced how to build and host a llm application using chat elemnets from streamlit.
+- I practiced how to use Streamlit session states to store and display the dynamic messages.
 
-## Questions
-- I recieved App Service Validation Error when selecting US West 1 and West 2. The exact error is as below:
-```
-{"code":"InvalidTemplateDeployment","details":[{"code":"ValidationForResourceFailed","message":"Validation failed for a resource. Check 'Error.Details[0]' for more information.","details":[{"code":"SubscriptionIsOverQuotaForSku","message":"This region has quota of 0 instances for your subscription. Try selecting different region or SKU."}]}],"message":"The template deployment 'Microsoft.Web-FunctionApp-Portal-01b72bda-996c' is not valid according to the validation procedure. The tracking id is 'ed86df9b-5636-4adf-9e6c-4659d4ae6852'. See inner errors for details."}
-```
-
-The error disappears when selecting West 3 and the paid app plan B1. I'm still not sure how to solve this issue.
+## Questions (Solved)
+- I encountered session not initialized error when I forgot to initialize the chat history session. 
--- app.py
+++ app.py
@@ -1,102 +1,74 @@
 import streamlit as st
+from dotenv import load_dotenv
+import os
+from llama_index.core import VectorStoreIndex
+from llama_index.llms.openai import OpenAI
+from llama_index.readers.file import PDFReader
+from tempfile import NamedTemporaryFile
+
+load_dotenv()
 
 def main():
-    st.title("Kejuan Yang's Website")
-
-    # Columns for About Section and Profile Picture
-    col1, col2 = st.columns(2)
-
-    # About Section in the left column
-    with col1:
-        st.header("About")
-        st.write("Hi there! My name is Kejuan Yang. I am currently pursuing my dual-degree master's in Computer Science at the University of Washington, in collaboration with Tsinghua University, where I specialize in Natural Language Processing and Machine Learning. ")
-        st.write("I have a keen interest in agent-driven AI, as I have worked as research assistant in Large Language Model Evaluation. Feel free to connect with me for any collaborative opportunities or just to exchange insights about the world of AI!")
-    # Profile Picture in the right column
-    with col2:
-        for _ in range(5):
-            st.write("")
-
-        st.image("imgs/profile.JPG", width=280)
-        
-        
-    # Education Section
-    st.header("Education")
+    st.set_page_config(
+        page_title="AI Resume",
+        page_icon="📄",
+        layout="centered",
+        initial_sidebar_state="auto",
+        menu_items=None,
+    )
     
-    st.markdown("""
-    #### University of Washington
-    **M.S. in Computer Science, Global Innovation eXchange**  
-    **GPA:** 4.0/4.0  
-    **Duration:** Sep 2023 - Mar 2025
-    """, unsafe_allow_html=True)
+    if "messages" not in st.session_state.keys():  # Initialize the chat messages history
+        st.session_state.messages = [
+            {"role": "assistant", "content": "Ask me a question about your resume!"}
+        ]
 
-    st.markdown("""
-    #### Tsinghua University
-    **M.S. in Data Science and Information Technology, Global Innovation eXchange**  
-    **GPA:** 3.87/4.0  
-    **Duration:** Sep 2022 - Mar 2025
-    """, unsafe_allow_html=True)
+    st.title("AI Resume Bot 🤖📄")
+    st.write("Upload your resume or cover letter to get instant feedback.")
 
-    st.markdown("""
-    #### Beijing Institute of Technology
-    **Bachelor’s in Automation**  
-    **GPA:** 90/100  
-    **Duration:** Sep 2018 - Jun 2022
-    """, unsafe_allow_html=True)
+    uploaded_file = st.file_uploader("Upload your Resume or Cover Letter", type=["pdf", "docx", "txt"])
 
-    # Work Experience Section
-    st.header("Work Experience")
+    if uploaded_file:
+        file_type = uploaded_file.type
+        bytes_data = uploaded_file.read()
+        with NamedTemporaryFile(delete=False, suffix=f".{file_type.split('/')[-1]}") as tmp:
+            tmp.write(bytes_data)
 
-    # ZhipuAI
-    st.markdown("""
-    #### ZhipuAI Co., China
-    **Position:** Research Assistant, ChatGLM Large Language Model  
-    **Duration:** Mar 2023 - Aug 2023  
-    - Researched innovative applications of LLM in web browsing tasks like ticketing and online shopping.
-    - Developed a web application supporting user instruction in Python, integrating with LLM for action planning.
-    - Designed a visual interface for task execution, service logic for web content interaction, and benchmarking tools.
-    """, unsafe_allow_html=True)
+            reader = PDFReader()
+            docs = reader.load_data(tmp.name)
+            llm = OpenAI(
+                model="gpt-3.5-turbo",
+                temperature=0.0,
+                system_prompt="You are an expert on the content of the document, provide detailed answers to the questions. Use the document to support your answers.",
+            )
+            index = VectorStoreIndex.from_documents(docs)
+        
+        os.remove(tmp.name)  # remove temp file after processing
 
-    # BIGAI
-    st.markdown("""
-    #### Beijing Institute of General Artificial Intelligence, China
-    **Position:** Research Assistant, Multiagent  
-    **Duration:** Oct 2021 - Jun 2022
-    - Developed a simulation platform for robotic manipulation and interaction using C# and Unity3D.
-    - Implemented algorithms for articulated container control and obstacle-avoidance path planning.
-    - Integrated VR interaction for first-view object interaction, supporting the BigScene project.
-    """, unsafe_allow_html=True)
+        if "chat_engine" not in st.session_state.keys():  # Initialize the chat engine
+            st.session_state.chat_engine = index.as_chat_engine(
+                chat_mode="condense_question", verbose=False, llm=llm
+            )
+        else:
+            st.write("Please upload a file to get feedback.")
 
-    # Interests Section
-    st.header("Interests")
-    st.write("I love badminton, guitar and science fiction movies. Feel free to join me if you share the same interests!")
 
-    # Projects Section
-    st.header("Projects")
-    st.markdown("""
-    #### National Computer Games Competition - International Checkers 64 Squares
-    **Date:** Aug 2021  
-    **Description:**  
-    - Won National Champion
-    - Developed a chess-playing program using C++ with advanced search algorithms like Alpha-Beta pruning and Principal Variation Search.
-    - Enhanced decision-making efficiency during gameplay through multithreading, evaluating chess positions, moves, and strategies.
-    """, unsafe_allow_html=True)
+    if prompt := st.chat_input(
+        "Your question"
+    ):  # Prompt for user input and save to chat history
+        st.session_state.messages.append({"role": "user", "content": prompt})
 
-    st.markdown("""
-    #### Campus Library User Management Application
-    **Date:** Jun 2021  
-    **Description:**  
-    - Built a user registration and login interface utilizing Vue.js and established communication with the backend database.
-    - Successfully deployed the application across the school, achieving over 2000 users.
-    """, unsafe_allow_html=True)
+    for message in st.session_state.messages:  # Display the prior chat messages
+        with st.chat_message(message["role"]):
+            st.write(message["content"])
 
-    st.markdown("""
-    #### National Robocup Competition in 3D Recognition
-    **Date:** Nov 2020  
-    **Description:**  
-    - Won National First Prize
-    - Developed a recognition application within the ROS framework, facilitating message transmission and retrieval.
-    - Implemented real-time detection and localization of daily objects with a 91% accuracy rate using the YOLOv3 algorithm.
-    """, unsafe_allow_html=True)
+    # If last message is not from assistant, generate a new response
+    if st.session_state.messages[-1]["role"] != "assistant":
+        with st.chat_message("assistant"):
+            with st.spinner("Thinking..."):
+                response = st.session_state.chat_engine.stream_chat(prompt)
+                st.write_stream(response.response_gen)
+                message = {"role": "assistant", "content": response.response}
+                st.session_state.messages.append(message)  # Add response to message history
 
 
 if __name__ == "__main__":
